a6c7258be72863520c36b86e94a9410eefdb9781
geekhall <174821300@qq.com>
2022-02-10 13:35:12 +0800

    add commit shell

---
+++ commit.sh
@@ -0,0 +1,13 @@
+#########################################################
+# filename : commit.sh
+# author   : geekhall
+# version  : v1.0.0
+# function : quick commit tool
+#            push to github, gitee and gitlab.
+#########################################################
+git push github master
+git push gitee master
+git push gitlab master
+echo "commit ok"
+
+
